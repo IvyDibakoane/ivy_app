77e8017ca03cd6fa4a3b3480eb012264846bda0d
IvyDibakoane <mankolele@gmail.com>
2012-04-11 15:25:31 +0200

    ivyApp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IvyApp::Application.config.secret_token = '7a83313e31e7f4b65689a71ce1cf811dc6c3cc73fde9eb08c5a547f275c9e56be1aee8d396a0dff540156aa70cf01d219112d37804e9db45a0dcd0ea622148ca'
